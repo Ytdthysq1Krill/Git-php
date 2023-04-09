d418a69f51db166dbcdbcfcae76960956a6791e1
Kirill-1-chsu <Tutankhamen234@mail.ru>
2023-04-09 18:01:35 +0300

    change file 1

--- UserController.php
+++ UserController.php
@@ -20,7 +20,7 @@ function registerAction(){
     $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
     $email = trim($email);
 
-    $pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+    $password = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
     $pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
 
     $phone  = isset($_REQUEST['phone'])  ? $_REQUEST['phone']  : null;
